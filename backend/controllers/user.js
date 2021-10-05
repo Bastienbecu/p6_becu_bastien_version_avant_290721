8f2858fc6b8cb49ff0bbe857952d45bb422daf37
Bastienbecu <66145114+Bastienbecu@users.noreply.github.com>
2021-10-05 20:02:00 +0200

    Update user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -34,7 +34,6 @@ exports.signup = (req, res, next) => {
 
 };
 
-// Le Middleware pour la connexion d'un utilisateur vérifie si l'utilisateur existe dans la base MongoDB lors du login
 //si oui il vérifie son mot de passe, s'il est bon il renvoie un TOKEN contenant l'id de l'utilisateur, sinon il renvoie une erreur
 exports.login = (req, res, next) => {
   // On doit trouver l'utilisateur dans la BDD qui correspond à l'adresse entrée par l'utilisateur
@@ -84,54 +83,4 @@ exports.login = (req, res, next) => {
       error
     }));
 };
-/*
-exports.signup = (req, res, next) => {
-  //regex pour exiger un mot de passe fort d'au moins 8 caractères
-  //const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z0-9\d@$!%*?&]{8,}$/; 
-  const password = req.body.password;
- // const cryptedEmail = cryptojs.HmacSHA256(req.body.email, process.env.EMAIL_ENCRYPTION_KEY).toString();
 
-  if (password.match()) {
-  bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-      const user = new User({
-        email: req.body.email,
-        password: hash
-      });
-      user.save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-        .catch(error => res.status(400).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
-  } else {
-    throw new Error("Le mot de passe n'est pas assez sécurisé");
-  }
-};
-
-exports.login = (req, res, next) => {
-  //const cryptedEmail = cryptojs.HmacSHA256(req.body.email, process.env.EMAIL_ENCRYPTION_KEY).toString();
-
-  User.findOne({ email: req.body.email})
-    .then(user => {
-      if (!user) {
-        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-      }
-      bcrypt.compare(req.body.password, user.password)
-        .then(valid => {
-          if (!valid) {
-            return res.status(401).json({ error: 'Mot de passe incorrect !' });
-          }
-          res.status(200).json({
-            //encodage et renvoi au frontend d'un nouveau token contenant le userId en tant que payload
-            userId: user._id,
-            token: jwt.sign(
-              { userId: user._id },
-              'RANDOM_TOKEN_SECRET',
-              { expiresIn: '24h' }
-            )
-          });
-        })
-        .catch(error => res.status(500).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
-};*/
